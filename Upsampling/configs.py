35b3294cbff1901f10c48746c87d8f771b4df298
Nostalogicwh <apocalypsewh@163.com>
2022-02-16 10:57:11 +0800

    gpu

--- Upsampling/configs.py
+++ Upsampling/configs.py
@@ -56,6 +56,6 @@ parse.add_argument('--uniform_w', default=10.0, type=float, help="uniform_weight
 # gan损失权重
 parse.add_argument('--gan_w', default=0.5, type=float, help="gan_weight")
 parse.add_argument('--gen_update', default=2, type=int, help="gen_update")
-parse.add_argument('--gpu', default=0, type=str, help='choose_gpu')
+parse.add_argument('--gpu', default='0', type=str, help='choose_gpu')
 
 FLAGS = parse.parse_args()
